ffadc5eae112bf5f02270a1f2ffb1e92addb523b
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-29 23:50:36 +0300

    RecipeDetails screen layout

--- src/screens/RecipeDetails/index.js
+++ src/screens/RecipeDetails/index.js
@@ -1,14 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import Title from '../../components/Title'
 
 const RecipeDetails = ({route}) => {
-  const {name, thumbnail_url} = route.params.item
+  const {name, thumbnail_url, nutrition} = route.params.item
+  delete nutrition?.updated_at
+  const nutritionKeys = Object.keys(nutrition)
+  console.log("nutritionKeys :>> ", nutritionKeys)
 
   return (
     <SafeAreaView style={styles.container}>
+      <Image source={{uri: thumbnail_url}} style={styles.image}/>
       <Title text={name} />
+      {nutritionKeys?.map(key => (
+        <View style={styles.row} key={key}>
+            <Text style={styles.key}>{key}</Text>
+            <Text style={styles.value}>{nutrition[key]}</Text>
+        </View>
+      ))}
     </SafeAreaView>
   )
 }
--- src/screens/RecipeDetails/styles.js
+++ src/screens/RecipeDetails/styles.js
@@ -1,5 +1,34 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 
 export default styles = StyleSheet.create({
-    
+    container: {
+        paddingHorizontal: 24,
+        flex: 1
+    },
+    image: {
+        width: '100%',
+        height: 180,
+        borderRadius: 10,
+        marginBottom: 12
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(217,217,217,0.5)',
+        padding: 10,
+        paddingHorizontal: 12,
+        borderRadius: 8,
+        marginVertical: 4,
+    },
+    key: {
+        fontSize: 12,
+        color: colors.black,
+        textTransform: 'capitalize'
+    },
+    value: {
+        fontSize: 12,
+        color: colors.lightGrey2,
+    }
 })
