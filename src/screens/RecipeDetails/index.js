c39b43034ccfe1de984f6b93577ce244ec5aa2c6
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-30 16:33:04 +0300

    Finished the RecipeDetails screen

--- src/screens/RecipeDetails/index.js
+++ src/screens/RecipeDetails/index.js
@@ -1,24 +1,36 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import Title from '../../components/Title'
 
 const RecipeDetails = ({route}) => {
-  const {name, thumbnail_url, nutrition} = route.params.item
+  const {name, thumbnail_url, nutrition, instructions} = route.params.item
   delete nutrition?.updated_at
-  const nutritionKeys = Object.keys(nutrition)
+  const nutritionKeys = Object.keys(nutrition || {})
   console.log("nutritionKeys :>> ", nutritionKeys)
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={{uri: thumbnail_url}} style={styles.image}/>
-      <Title text={name} />
-      {nutritionKeys?.map(key => (
-        <View style={styles.row} key={key}>
-            <Text style={styles.key}>{key}</Text>
-            <Text style={styles.value}>{nutrition[key]}</Text>
-        </View>
-      ))}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image source={{uri: thumbnail_url}} style={styles.image}/>
+        <Title text={name} />
+        {nutritionKeys?.map(key => (
+          <View style={styles.row} key={key}>
+              <Text style={styles.key}>{key}</Text>
+              <Text style={styles.value}>{nutrition[key]}</Text>
+          </View>
+        ))}
+        <Title style={{marginTop: 32, marginBottom: 16}} text={'Instructions'}/>
+        {instructions?.map((instruction, index) => (
+          <View style={styles.instructionRow} key={instruction?.id}>
+              <Text style={styles.index}>{index + 1}</Text>
+              <Text style={styles.instructionText}>{instruction?.display_text}</Text>
+          </View>
+        ))}
+        {!instructions?.length? (
+          <Text style={styles.value}>No instructions found!</Text>
+        ): null}
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- src/screens/RecipeDetails/styles.js
+++ src/screens/RecipeDetails/styles.js
@@ -30,5 +30,20 @@ export default styles = StyleSheet.create({
     value: {
         fontSize: 12,
         color: colors.lightGrey2,
+    },
+    instructionRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 12,
+    },
+    index: {
+        fontSize: 20,
+        color: colors.grey2,
+        marginRight: 16,
+    },
+    instructionText: {
+        fontSize: 14,
+        color: colors.black,
+        flex:1,
     }
 })
